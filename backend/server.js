c494f2dcd1fe15d85149c5de807a91c78fa79061
David Zim <davidzim29.90@gmail.com>
2023-01-22 12:49:31 +0200

    start deploy

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+// app.use(express.json())
+
+app.get('/api', (req, res) => {
+    res.json({res: "hello"})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(PORT));
