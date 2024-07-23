6aecfe3d4bbab9c86d76ddd806d2a1e45f5e7c20
david-zim <davidzim29.90@gmail.com>
2024-07-23 15:55:28 +0200

    Add - Allow to provide the email and password through the url in the register page.

--- frontend/src/pages/registerPage/registerPage.tsx
+++ frontend/src/pages/registerPage/registerPage.tsx
@@ -1,5 +1,5 @@
-import {useNavigate} from "react-router-dom";
-import React, {useContext, useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import React, {useContext, useEffect, useState} from "react";
 import store from "../../store";
 import {observer} from "mobx-react";
 import {PopupContext} from "../../components/popupMessage/popupMessage";
@@ -10,8 +10,15 @@ interface serverResults {
     token: string;
 }
 
+const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+};
+
 function RegisterPage() {
     const navigate = useNavigate();
+    const query: URLSearchParams = useQuery();
+    const emailInUrl: string | null = query.get('email');
+    const passwordUrl: string | null = query.get('password');
     const popupContext = useContext(PopupContext);
 
     const [email, setEmail] = useState<string>('');
@@ -48,6 +55,13 @@ function RegisterPage() {
             }
         });
     }
+
+    useEffect(() => {
+        if (emailInUrl && passwordUrl) {
+            setEmail(emailInUrl);
+            setPassword(passwordUrl);
+        }
+    }, []);
 
     return (
         <div className="register-page">
